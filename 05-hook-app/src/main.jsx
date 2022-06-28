b663f4e769edbccf3005762d36fe92bae255a092
John Doe <john@doe.com>
2022-06-28 16:24:14 -0600

    Finaliza useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { HooksApp } from './HooksApp'
-import './index.css'
-import "bootstrap/dist/css/bootstrap.min.css";
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
@@ -21,5 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   //<SimpleForm />
   //<MultipleCustomHooks />
   //<FocusScreen />
-  <Layout />
+  //<Layout />
+  //<Memorize />
+  <MemoHook />
 )
