f90ef5cdd331f67ed2ef85da7eae83c64e40c108
Edgard Barquero <ebarquero85@gmail.com>
2022-06-29 15:06:07 -0600

    Se agrega el  main

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,34 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { HooksApp } from './HooksApp'
-import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
-import { Padre } from './07-tarea-memo/Padre';
+import { TodoApp } from './08-useReducer/TodoApp';
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+// import { Padre } from './07-tarea-memo/Padre';
+// import '../src/08-useReducer/intro-reducer';
 
-import '../src/08-useReducer/intro-reducer';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//   // </React.StrictMode>
-//   // <HooksApp />  
-//   // <CounterApp />
-//   // <CounterWithCustomHook />
-//   //<FormWithCustomHook />
-//   //<SimpleForm />
-//   //<MultipleCustomHooks />
-//   //<FocusScreen />
-//   //<Layout />
-//   //<Memorize />
-//   //<MemoHook />
-//   //<CallbackHook />
-//   //<Padre />
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>
+)
