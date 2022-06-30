42de11330459727039d5269292d9ef465c4a4921
Edgard Barquero <ebarquero85@gmail.com>
2022-06-30 15:10:03 -0600

    Finaliza sección #10

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,72 +1,19 @@
-import { TodoAdd } from './';
+import { useTodo } from '../hooks';
+import { TodoList, TodoAdd } from './';
 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Estudiar React',
-    //     done: false
-    // },
-    // {
-    //     id: new Date().getTime() + 1000,
-    //     description: 'Practicar React Native',
-    //     done: false
-    // },
-];
-
-const init = () => JSON.parse( localStorage.getItem('todos') || [] );
 
 export const TodoApp = () => {
 
-    // useTodo
-    // todo, handleNewTodo, handleRemoveTodo, onToggleTodo
-    // 
-
-    const [ todos, dispatch ] = useReducer(todoReducer, initialState, init);
-
-    useEffect(() => {
-
-        localStorage.setItem('todos', JSON.stringify(todos) || []);
-
-    }, [todos]);
-
-
-    const handleNewTodo = ( todo ) => {
-        
-        const action = {
-            type: '[TODO] Add todo',
-            payload: todo
-        }
-
-        dispatch( action );
-
-    };
-
-    const handleRemoveTodo = ( id ) => {
-
-        dispatch({
-            type: '[TODO] Remove todo',
-            payload: id
-        })
-        
-    };
-
-    const onToggleTodo = ( id ) => {
-        
-        dispatch({
-            type: '[TODO] Toggle todo',
-            payload: id
-        })
-
-    }
-
-
+    const { todos, todoCount, todoPending, handleNewTodo, handleRemoveTodo, onToggleTodo } = useTodo();
  
     return (
         <>
-            <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
+            <h1>TodoApp: { todoCount }, <small>pendientes: { todoPending }</small></h1>
             <hr />
 
             <div className="row">
+                
+                {/* COLUMNA IZQUIERDA */}
                 <div className="col-7">
 
                     <TodoList 
@@ -76,15 +23,20 @@ export const TodoApp = () => {
                     />
 
                 </div>
+
+                {/* COLUMNA DERECHA */}
                 <div className="col-5">
 
                     <h4>Agregar TODO</h4>
                     <hr />
 
-                    <TodoAdd handleNewTodo={ handleNewTodo } />
+                    <TodoAdd 
+                        handleNewTodo={ handleNewTodo } 
+                    />
 
                 </div>
             </div>
         </>
     )
+
 }
--- 05-hook-app/src/hooks/useTodo.js
+++ 05-hook-app/src/hooks/useTodo.js
@@ -1,11 +1,71 @@
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
 
-export const useTodo = () => {
+// const initialState = [
+//     {
+//         id: new Date().getTime(),
+//         description: 'Estudiar React',
+//         done: false
+//     },
+//     {
+//         id: new Date().getTime() + 1000,
+//         description: 'Practicar React Native',
+//         done: false
+//     },
+// ];
+
+const init = () => JSON.parse( localStorage.getItem('todos') || [] );
+
+export const useTodo = (initialState = []) => {
+
+    const [ todos, dispatch ] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+
+        localStorage.setItem('todos', JSON.stringify(todos) || []);
+
+    }, [todos]);
 
 
+    const handleNewTodo = ( todo ) => {
+        
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+
+        dispatch( action );
+
+    };
+
+    const handleRemoveTodo = ( id ) => {
+
+        dispatch({
+            type: '[TODO] Remove todo',
+            payload: id
+        })
+        
+    };
+
+    const onToggleTodo = ( id ) => {
+        
+        dispatch({
+            type: '[TODO] Toggle todo',
+            payload: id
+        })
+
+    }
+
+    const getPendientes = () => todos.filter( todo => todo.done === false ).length;
 
 
     return {
-    
+        todos,
+        todoCount: todos.length,
+        todoPending: getPendientes(),
+        handleNewTodo,
+        handleRemoveTodo,
+        onToggleTodo,
     }
+    
 }
