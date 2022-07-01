68e66f6081d5ce63a33fbe64d1787936229761b4
John Doe <john@doe.com>
2022-06-30 19:03:37 -0600

    Finaliza clase #158

--- 05-hook-app/src/09-useContext/Main.jsx
+++ 05-hook-app/src/09-useContext/Main.jsx
@@ -2,11 +2,15 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { HomePage } from './HomePage';
 import { AboutPage } from './AboutPage'
 import { LoginPage } from './LoginPage'
+import { NavBar } from './NavBar';
 
 export const Main = () => {
     return (
         <>
             <h1>MainApp</h1>
+
+            <NavBar />
+            
             <hr />
 
             <Routes>
